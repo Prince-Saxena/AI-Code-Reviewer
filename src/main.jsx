3f86e711a6a5ba7827e9580aa6784ddee34293b3
Prince Saxena <princesaxena202020@gmail.com>
2025-02-15 12:03:22 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { MarkdownProvider } from "./context/Markdown.context.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<MarkdownProvider>
+		<App />
+	</MarkdownProvider>
+);
